cc15480d4d3e837bb9ce26dc358527f1cb9e028d
CorvusChrome <rentinchibs@gmail.com>
2020-02-09 17:38:34 +0200

    for safety reasons - place vector on the floor

--- Program.cs
+++ Program.cs
@@ -16,19 +16,37 @@ namespace RobotTests
             await using var robot = await RobotScheduler.Connect();
             robot.AddBehavior(ParrotModeScreen);
             robot.AddBehavior(ParrotModeWheels);
+            robot.AddBehavior(ParrotModeLift);
+            robot.AddBehavior(ParrotModeHead);
             Console.ReadKey();
         }
+        static async Task ParrotModeLift(Robot robot, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await robot.Motors.MoveLift(MoveDirection.Up, MotorSpeed.Slow);
+                await Task.Delay(500, token);
+                await robot.Motors.MoveLift(MoveDirection.Down, MotorSpeed.Fast);
+                await robot.Motors.MoveLift(MoveDirection.Up, MotorSpeed.Fast);
+                await robot.Motors.MoveLift(MoveDirection.Down, MotorSpeed.Fast);
+            }
+        }
+        static async Task ParrotModeHead(Robot robot, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await robot.Motors.MoveHead(MoveDirection.Up, MotorSpeed.Slow);
+                await Task.Delay(500, token);
+                await robot.Motors.MoveHead(MoveDirection.Down, MotorSpeed.Fast);
 
+            }
+        }
         static async Task ParrotModeWheels(Robot robot, CancellationToken token)
         {
             while (!token.IsCancellationRequested)
             {
-                await robot.Motors.SetWheelMotors(50, -50);
-                await robot.Motors.MoveLift(MoveDirection.Up, MotorSpeed.Medium);
-                await Task.Delay(500, token);
-                await robot.Motors.SetWheelMotors(-50, 50);
-                await robot.Motors.MoveLift(MoveDirection.Down, MotorSpeed.Medium);
-                await Task.Delay(500, token);
+                await Task.Delay(1000, token);
+                await robot.Motors.SetWheelMotors(100, -100);
             }
         }
 
@@ -36,15 +54,25 @@ namespace RobotTests
         {
             while (!token.IsCancellationRequested)
             {
+                await robot.Screen.DisplaySolidColor(Color.Black, 0, false);
+                await Task.Delay(500, token);
+                await robot.Screen.DisplaySolidColor(Color.Red, 0, false);
+                await Task.Delay(200, token);
+                await robot.Screen.DisplaySolidColor(Color.Black, 0, false);
+                await Task.Delay(200, token);
                 await robot.Screen.DisplaySolidColor(Color.Red, 0, false);
                 await Task.Delay(100, token);
-                await robot.Screen.DisplaySolidColor(Color.Yellow, 0, false);
+                await robot.Screen.DisplaySolidColor(Color.Red, 0, false);
+                await Task.Delay(200, token);
+                await robot.Screen.DisplaySolidColor(Color.Black, 0, false);
+                await Task.Delay(200, token);
+                await robot.Screen.DisplaySolidColor(Color.Red, 0, false);
                 await Task.Delay(100, token);
-                await robot.Screen.DisplaySolidColor(Color.Green, 0, false);
-                await Task.Delay(100, token);
-                await robot.Screen.DisplaySolidColor(Color.Blue, 0, false);
-                await Task.Delay(100, token);
-                await robot.Screen.DisplaySolidColor(Color.White, 0, false);
+                await robot.Screen.DisplaySolidColor(Color.Red, 0, false);
+                await Task.Delay(200, token);
+                await robot.Screen.DisplaySolidColor(Color.Black, 0, false);
+                await Task.Delay(200, token);
+                await robot.Screen.DisplaySolidColor(Color.Red, 0, false);
                 await Task.Delay(100, token);
             }
         }
