a73f5eb82badd723bb3fa1406bbabab98eedad1a
RandoM RandoM <dmytroz@tradeix.com>
2020-02-09 15:20:52 +0200

    Added RobotScheduler

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using Anki.Vector;
+using Google.Type;
+using Color = Anki.Vector.Types.Color;
+
+namespace RobotTests
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            await using (var robot = await RobotScheduler.Connect())
+            {
+                robot.AddBehavior(ParrotModeScreen);
+                robot.AddBehavior(ParrotModeWheels);
+                Console.ReadKey();
+            }
+        }
+
+        static async Task ParrotModeWheels(Robot robot, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await robot.Motors.SetWheelMotors(100, -100);
+                await Task.Delay(500, token);
+                await robot.Motors.SetWheelMotors(-100, 100);
+                await Task.Delay(500, token);
+            }
+        }
+
+        static async Task ParrotModeScreen(Robot robot, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await robot.Screen.DisplaySolidColor(Color.Blue, 0, false);
+                await Task.Delay(200, token);
+                await robot.Screen.DisplaySolidColor(Color.Red, 0, false);
+                await Task.Delay(200, token);
+                await robot.Screen.DisplaySolidColor(Color.Green, 0, false);
+                await Task.Delay(200, token);
+            }
+        }
+    }
+}
---
+++ RobotScheduler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Anki.Vector;
+
+namespace RobotTests
+{
+    public class RobotScheduler : IAsyncDisposable
+    {
+        private Robot _robot;
+        private List<Task> _tasks = new List<Task>();
+        private CancellationTokenSource _tokenSource;
+
+        private RobotScheduler()
+        {
+        }
+
+        public static async Task<RobotScheduler> Connect()
+        {
+            var scheduler = new RobotScheduler();
+            await scheduler.ConnectInernal();
+            return scheduler;
+        }
+
+        private async Task ConnectInernal()
+        {
+            _robot = await Robot.NewConnection();
+            _tokenSource = new CancellationTokenSource();
+            await _robot.Control.RequestControl();
+        }
+
+        public void AddBehavior(Func<Robot, CancellationToken, Task> fn)
+        {
+            var task = Task.Run(() => fn(_robot, _tokenSource.Token), _tokenSource.Token);
+            _tasks.Add(task);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            try
+            {
+                _tokenSource.Cancel();
+                await Task.WhenAll(_tasks);
+                await _robot.Motors.StopAllMotors();
+            }
+            finally
+            {
+                _robot.Dispose();
+                _tokenSource.Dispose();
+            }
+        }
+    }
+}
